e2c0fd2cc0058151fa0b6ae56f3a3152b239ca2b
Matteo Monti <matteo.monti@rain.vg>
2017-05-16 23:50:22 +0200

    Update main.js
    Re-activates server on main.

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var rendezvous = require('./peer/rendezvous.js');
 
 (async function()
 {
-  //var my_server = new server();
-  //await my_server.serve();
+  var my_server = new server();
+  await my_server.serve();
 
   var my_rendezvous = new rendezvous();
   my_rendezvous.serve();
